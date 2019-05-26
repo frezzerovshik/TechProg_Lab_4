456f44b56edb201431b459cdff994fbfb82c0cc1
frezzerovshik <43182862+frezzerovshik@users.noreply.github.com>
2019-05-26 20:00:20 +0300

    Add files via upload

---
+++ merge_sort.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+template <typename T>
+
+void merge_sort(T *A, int first, int last)
+{
+	if (first == last || last == 0)
+		throw 666;
+	int middle, start, final, j;
+	T *mas = new T[10];
+	middle = (first + last) / 2; 
+	start = first; 
+	final = middle + 1; 
+	{
+		if (first < last)
+		{
+			merge_sort(A, first, (first + last) / 2); 
+			merge_sort(A, (first + last) / 2 + 1, last); 
+			for (j = first; j <= last; j++)
+				if ((start <= middle) && ((final > last) || (A[start] < A[final])))
+				{
+					mas[j] = A[start];
+					start++;
+				}
+				else
+				{
+					mas[j] = A[final];
+					final++;
+				}
+			for (j = first; j <= last; j++) A[j] = mas[j];
+			delete[]mas;
+		}
+	}
+};
---
+++ node.hpp
@@ -0,0 +1,19 @@
+//
+//  int_node.hpp
+//  T.P.L.B.4
+//
+//  Created by Артем Шарапов on 15/05/2019.
+//  Copyright © 2019 Артем Шарапов. All rights reserved.
+//
+#pragma once
+template<typename T>
+class Node
+{
+	T Value;
+	Node *Previous;
+public:
+	T getdata() { return Value; }
+	Node* getnext() { return Previous; }
+	void setdata(int a) { Value = a; }
+	void setnext(Node* a) { Previous = a; }
+};
